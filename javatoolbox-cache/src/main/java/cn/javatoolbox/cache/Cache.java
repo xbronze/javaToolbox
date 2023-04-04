4d349375d3529fea7be023d4ea2e6ae69b929a69
xbronze <zihui_xu@126.com>
2023-04-04 19:32:55 +0800

    新增最少使用策略缓存

---
+++ javatoolbox-cache/src/main/java/cn/javatoolbox/cache/Cache.java
@@ -0,0 +1,46 @@
+package cn.javatoolbox.cache;
+
+import lombok.Data;
+
+/**
+ * @author: xbronze
+ * @date: 2023-04-04 17:02
+ * @description: 缓存实体
+ */
+@Data
+public class Cache implements Comparable<Cache>{
+
+    /**
+     * 键
+     */
+    private Object key;
+
+    /**
+     * 缓存值
+     */
+    private Object value;
+
+    /**
+     * 最后一次访问时间
+     */
+    private long accessTime;
+
+    /**
+     * 创建时间
+     */
+    private long writeTime;
+
+    /**
+     * 存活时间
+     */
+    private long expireTime;
+
+    /**
+     * 命中次数
+     */
+    private Integer hitCount;
+
+    public int compareTo(Cache o) {
+        return this.hitCount.compareTo(o.getHitCount());
+    }
+}
---
+++ javatoolbox-cache/src/main/java/cn/javatoolbox/cache/LFUCache.java
@@ -0,0 +1,150 @@
+package cn.javatoolbox.cache;
+
+import java.util.Collections;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author: xbronze
+ * @date: 2023-04-04 17:12
+ * @description: 最少使用策略的缓存
+ */
+public class LFUCache<K, V> {
+
+    // 缓存载体
+    private ConcurrentHashMap<Object, Cache> concurrentHashMap;
+
+    /**
+     * 最大缓存
+     */
+    private final int size;
+
+    public LFUCache(int size) {
+        this.size = size;
+        this.concurrentHashMap = new ConcurrentHashMap<Object, Cache>(size);
+        new Thread(new TimeoutTimerThread()).start();
+    }
+
+    /**
+     * 获取缓存
+     * @param key 缓存key
+     * @return 缓存内容
+     */
+    public Object get(K key) {
+        checkNotNull(key);
+        if (concurrentHashMap.isEmpty()) {
+            return null;
+        }
+        if (concurrentHashMap.containsKey(key)) {
+            Cache cache = concurrentHashMap.get(key);
+            if (cache == null) {
+                return null;
+            }
+            cache.setHitCount(cache.getHitCount() + 1);
+            cache.setAccessTime(System.currentTimeMillis());
+            return cache.getValue();
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * 添加缓存
+     * @param key
+     * @param value
+     * @param expire
+     */
+    public void put(K key, V value, long expire){
+        checkNotNull(key);
+        checkNotNull(value);
+        // 当缓存存在时，更新缓存
+        if (concurrentHashMap.containsKey(key)) {
+            Cache cache = concurrentHashMap.get(key);
+            // 命中次数加1
+            cache.setHitCount(cache.getHitCount() + 1);
+            // 更新写入时间和最后一次访问时间
+            cache.setWriteTime(System.currentTimeMillis());
+            cache.setAccessTime(System.currentTimeMillis());
+            // 更新缓存的存活时间
+            cache.setExpireTime(expire);
+            // 更新缓存内容
+            cache.setValue(value);
+            return;
+        }
+
+        // 当缓存不存在，并且还没达到最大缓存数
+        if (isFull()) {
+            // 获取最少使用的缓存key
+            Object leastFrequentlyUserKey = getLeastFrequentlyUsedKey();
+            if (leastFrequentlyUserKey != null) {
+                concurrentHashMap.remove(leastFrequentlyUserKey);
+            }
+        }
+
+        Cache cache = new Cache();
+        cache.setKey(key);
+        cache.setValue(value);
+        cache.setWriteTime(System.currentTimeMillis());
+        cache.setAccessTime(System.currentTimeMillis());
+        cache.setHitCount(1);
+        cache.setExpireTime(expire);
+        concurrentHashMap.put(key, cache);
+    }
+
+    public static <T> T checkNotNull(T reference) {
+        if (reference == null) {
+            throw new NullPointerException();
+        }
+        return reference;
+    }
+
+    /**
+     * 获取最少使用的缓存key
+      */
+    private Object getLeastFrequentlyUsedKey() {
+        Cache minCache = Collections.min(concurrentHashMap.values());
+        if (minCache == null) {
+            return null;
+        }
+        return minCache.getKey();
+    }
+
+    /**
+     * 判断是否达到最大缓存数
+     * @return 是否最大缓存
+     */
+    private boolean isFull() {
+        return concurrentHashMap.size() == size;
+    }
+
+
+    /**
+     * 处理过期缓存
+     */
+    class TimeoutTimerThread implements Runnable {
+        public void run() {
+            while (true) {
+                try {
+                    TimeUnit.SECONDS.sleep(60);
+                    expireCache();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        /**
+         * 清除过期缓存
+         */
+        private void expireCache() {
+            for (Object key : concurrentHashMap.keySet()) {
+                Cache cache = concurrentHashMap.get(key);
+                long timeoutTime = TimeUnit.NANOSECONDS.toSeconds(System.nanoTime() - cache.getWriteTime());
+                if (timeoutTime > cache.getExpireTime()) {
+                    // 清除过期的缓存
+                    concurrentHashMap.remove(key);
+                }
+            }
+        }
+    }
+}
